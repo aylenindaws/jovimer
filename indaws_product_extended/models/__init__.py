7f3ca57a1771e6232789126ea155880d3366c05c
Daniers <michael@indaws.es>
2022-01-31 13:49:14 +0100

    Correccion de error de campos para modelos anexos

--- indaws_product_extended/models/__init__.py
+++ indaws_product_extended/models/__init__.py
@@ -6,8 +6,8 @@ from . import jovimer_confeccion
 from . import jovimer_envase
 from . import jovimer_etiquetas_plantilla
 from . import jovimer_etiquetas
-from . import jovimer_expedientes_series
-from . import jovimer_expedientes
+#from . import jovimer_expedientes_series
+#from . import jovimer_expedientes
 from . import jovimer_marca
 from . import jovimer_variedad
 from . import product_template
