f3082ffc652c6c3a024e39928e06f3ea13f4d4f7
Daniers <michael@indaws.es>
2022-01-31 21:00:50 +0100

    Correccion de errores

--- indaws_sale_purchase_extended/models/purchase_order.py
+++ indaws_sale_purchase_extended/models/purchase_order.py
@@ -10,5 +10,5 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     dossier_id = fields.Many2one('jovimer.expedientes', string='expediente', store=True, copy=True, ondelete='set null')
-    country_id = fields.Many2one(string="Pais Destino", comodel_name='res.country', required=True, default=lambda x: x.env.company.country_id.id, help="Country for which this report is available.")
+    country_id = fields.Many2one(string="Pais Destino", comodel_name='res.country', default=lambda x: x.env.company.country_id.id, help="Country for which this report is available.")
     close_sale = fields.Boolean(string='Pedido Cerrado')
