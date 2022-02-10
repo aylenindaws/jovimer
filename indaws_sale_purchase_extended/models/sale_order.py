6c5936c8db0da435533f6c95e26b9089d114ed47
Daniers <michael@indaws.es>
2022-02-10 20:11:00 +0100

    Correccion de error en los modulos

--- indaws_sale_purchase_extended/models/sale_order.py
+++ indaws_sale_purchase_extended/models/sale_order.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import subprocess
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -13,10 +14,17 @@ class SaleOrder(models.Model):
     palets = fields.Float(string='C. Compra', store=True, copy=True)
     paletsv = fields.Float(string='C. Venta', store=True, copy=True)
     platform = fields.Char(string='Plataforma')
-    edi_file = fields.Many2one('ir.attachment', string="Fichero EDI", store=True, copy=True, ondelete='set null')
+    edi_file = fields.Many2one('ir.attachment', string="Fichero EDI", store=True, copy=True, ondelete='set null', domain="[('mimetype','=','text/plain')]")
     date_end = fields.Date(string='Fecha de Salida')
     date_start = fields.Date(string='Fecha de Llegada')
     time_start = fields.Char(string='Hora de Llegada')
     conforms_lot = fields.Many2one('jovimer.conflote', string="Conforma LOTE", store=True, copy=True, ondelete='set null')
     lote_char = fields.Char(string='Lote', store=True, copy=True)
     close_sale = fields.Boolean(string='Pedido Cerrado')
+
+    @api.multi
+    def update_edi_file(self, default=None):
+        id = str(self.id)
+        args = ["/opt/jovimer12/bin/importaedi_pedido.sh", id, "&"]
+        subprocess.call(args)
+        return {}
