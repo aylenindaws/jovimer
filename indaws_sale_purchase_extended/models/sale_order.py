24a6eeab3d1a75b50500bf9acdb71c8a1f409e61
Daniers <michael@indaws.es>
2022-02-14 21:35:29 +0100

    Correccion de funcion de calculo para cargar EDI

--- indaws_sale_purchase_extended/models/sale_order.py
+++ indaws_sale_purchase_extended/models/sale_order.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import base64
 import os.path
 
 from odoo import models, fields, api
@@ -24,10 +25,45 @@ class SaleOrder(models.Model):
     close_sale = fields.Boolean(string='Pedido Cerrado')
 
     def update_edi_file(self, default=None):
-        id = str(self.id)
-        path = 'static/sh/importaedi_pedido.sh'
-        dir = os.path.dirname(__file__)
-        file_dir = dir.replace('models','')
-        args = [file_dir+path, id, "&"]
-        subprocess.call(args)
-        return {}
+        for item in self:
+            count = 0
+            aux_txt = base64.b64decode(item.edi_file.datas).decode('utf-8', 'ignore').split('\n')
+            item.order_line.unlink()
+            for linea in aux_txt:
+                count+=1
+                if count==1:
+                    if '96AORDERSP' in linea:
+                        _logger.info('Fichero EDI Valido para su procesamiento')
+                    else:
+                        _logger.info('Fichero EDI Valido para su procesamiento')
+                        id_edi_jovimer = linea[4:17]
+                        break
+                elif count==2:
+                    id_edi_cliente = linea[27:40]
+                    fecha_pedido = linea[18:26]
+                    fecha_llegada = linea[112:120]
+                else:
+                    product_id = self.env['product.template'].search([("partner_code", "=", linea[35:42])])
+                    quantity = linea[65:71]
+                    und = linea[72:75]
+                    product_description = linea[75:125]
+                    product_conv = linea[555]
+                    if 'CT' in und:
+                        id_und = 24
+                    elif 'PCE' in und:
+                        id_und = 1
+                    elif 'KGM' in und:
+                        id_und = 27
+                    else:
+                        id_und = 1
+                    item.order_line.create(
+                        {
+                            "order_id": item.id,
+                            "product_id": product_id.id,
+                            "product_uom_qty":quantity,
+                            "name":product_description,
+                            "price_unit": product_id.lst_price,
+                            'currency_id': 1,
+                        }
+                    )
+        return
