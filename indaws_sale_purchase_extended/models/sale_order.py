b94577271e8414ce979cd30a7cd4dad49c999278
Daniers <michael@indaws.es>
2022-02-15 16:31:34 +0100

    Agregar procesador de ficheros

--- indaws_sale_purchase_extended/models/sale_order.py
+++ indaws_sale_purchase_extended/models/sale_order.py
@@ -36,34 +36,37 @@ class SaleOrder(models.Model):
                         _logger.info('Fichero EDI Valido para su procesamiento')
                     else:
                         _logger.info('Fichero EDI Valido para su procesamiento')
-                        id_edi_jovimer = linea[4:17]
+                        id_edi_jovimer = linea[3:16]
                         break
                 elif count==2:
-                    id_edi_cliente = linea[27:40]
-                    fecha_pedido = linea[18:26]
-                    fecha_llegada = linea[112:120]
+                    id_edi_cliente = linea[26:39]
+                    fecha_pedido = linea[17:25]
+                    fecha_llegada = linea[111:119]
                 else:
-                    product_id = self.env['product.template'].search([("partner_code", "=", linea[35:42])])
-                    quantity = linea[65:71]
-                    und = linea[72:75]
-                    product_description = linea[75:125]
-                    product_conv = linea[555]
-                    if 'CT' in und:
-                        id_und = 24
-                    elif 'PCE' in und:
-                        id_und = 1
-                    elif 'KGM' in und:
-                        id_und = 27
-                    else:
-                        id_und = 1
-                    item.order_line.create(
-                        {
-                            "order_id": item.id,
-                            "product_id": product_id.id,
-                            "product_uom_qty":quantity,
-                            "name":product_description,
-                            "price_unit": product_id.lst_price,
-                            'currency_id': 1,
-                        }
-                    )
+                    if 'L' == linea[0:1]:
+                        product_id = self.env['product.template'].search([('partner_code', '=', linea[34:41])], limit=1)
+                        und = linea[72:75]
+                        product_description = linea[75:125]
+                        if 'CT' in und:
+                            id_und = 24
+                        elif 'PCE' in und:
+                            id_und = 1
+                        elif 'KGM' in und:
+                            id_und = 27
+                        else:
+                            id_und = 1
+                        if linea[65:71].replace(' ','') is '' or linea[65:71].replace(' ','') is False:
+                            quantity = 0
+                        else:
+                            quantity = float(linea[65:71].replace(' ',''))
+                        item.order_line.create(
+                            {
+                                "order_id": item.id,
+                                "product_id": product_id.id,
+                                "product_uom_qty":quantity,
+                                "name":product_description,
+                                "price_unit": product_id.lst_price if product_id.lst_price is not False else 0,
+                                'currency_id': 1,
+                            }
+                        )
         return
