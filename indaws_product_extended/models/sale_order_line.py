c43a4d769534d290bbea085ca2b943767c7d87a0
Daniers <Michael@indaws.es>
2022-03-02 14:02:46 -0500

    Correcciones de modulos de Compras y Ventas

--- indaws_product_extended/models/sale_order_line.py
+++ indaws_product_extended/models/sale_order_line.py
@@ -105,6 +105,7 @@ class ModelSaleOrderLine(models.Model):
     ], string='Estado', default='OK')
     partner_code = fields.Char(string='Codigo Cliente', related='etiqueta.partner_code')
     not_active = fields.Boolean('NO Activo', related='product_id.not_active')
+    uom_po_id = fields.Many2one('uom.uom', 'Ud Compra', required=True, help="Default unit of measure used for purchase orders. It must be in the same category as the default unit of measure.")
 
     def recalculalinea(self):
         variedad = self.product_id.variety
