52ba0396cf5c0a9acad3b7f361edc2eb6d7a3d94
Daniers <Michael@indaws.es>
2022-03-04 10:26:19 -0500

    Correccion de error de albaranes

---
+++ indaws_product_extended/models/res_partner.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import base64
+import os.path
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
+from datetime import datetime, date, time, timedelta
+import subprocess
+import logging
+import datetime
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    trasnportista = fields.Boolean(string='Transportista')
