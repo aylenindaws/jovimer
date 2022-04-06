bf4d6e3f3c100c12132782df449cb61b0300e500
Daniers <Michael@indaws.es>
2022-04-06 09:13:06 -0500

    Correccion de creacion de lineas de factura

--- indaws_product_extended/models/sale_order_line.py
+++ indaws_product_extended/models/sale_order_line.py
@@ -359,3 +359,24 @@ class ModelSaleOrderLine(models.Model):
             item.margin = sale - transport - purchase
             item.margin_percent = (item.margin * 100) / sale if sale != 0 else (item.margin * 100)
 
+
+    def _prepare_invoice_line(self, **optional_values):
+        res = super(ModelSaleOrderLine, self)._prepare_invoice_line(optional_values)
+        self.ensure_one()
+        raise ValidationError('Entra al Generador de lineas')
+        res[''] = {
+            'display_type': self.display_type,
+            'sequence': self.sequence,
+            'name': self.name,
+            'product_id': self.product_id.id,
+            'product_uom_id': self.product_uom.id,
+            'quantity': self.qty_to_invoice,
+            'discount': self.discount,
+            'price_unit': self.price_unit,
+            'tax_ids': [(6, 0, self.tax_id.ids)],
+            'analytic_account_id': self.order_id.analytic_account_id.id,
+            'analytic_tag_ids': [(6, 0, self.analytic_tag_ids.ids)],
+            'sale_line_ids': [(4, self.id)],
+        }
+        return res
+
